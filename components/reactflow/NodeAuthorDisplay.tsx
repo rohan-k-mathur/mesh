5d35248749776c79503a7c5fd31b9a0bb34c64d0
Rohan Mathur <rohan.kumar.mathur@gmail.com>
2025-06-24 17:21:29 -0700

    quik

--- components/reactflow/NodeAuthorDisplay.tsx
+++ components/reactflow/NodeAuthorDisplay.tsx
@@ -11,15 +11,15 @@ const NodeAuthorDisplay = ({ author }: Props) => {
   return (
     // Keep the Panel if you need it positioned via React Flow
     <Panel position="top-left">
-      <div className="pb-20">
+      <div className="pb-20 ">
         {/* This link wraps both the image and the username, so you have one unified hover area */}
-        <Link href={`/profile/${author.id}`} className="node-author-wrapper">
+        <Link href={`/profile/${author.id}`} className="node-author-wrapper ">
           <Image
             src={"image" in author ? author.image || "" : ""}
             alt="Profile Image"
             width={24}
             height={24}
-            className="node-profile-photo"
+            className="node-profile-photo "
           />
           <p className="node-profile-name">
             {"@" + ("username" in author ? author.username : "")}
