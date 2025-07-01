6533b622ad22fbf9cac0dbf9d613ad286400c88c
Rohan Mathur <rohan.kumar.mathur@gmail.com>
2025-06-30 19:25:46 -0700

    rightsidebar ui

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -48,10 +48,10 @@ function RightSidebar() {
       </div>
       <div className="flex w-full flex-1 flex-col gap-2 px-2">
         <h3 className="relative bottom-[4rem] text-[1.5rem] text-black">Find New Users</h3>
-        <div className="relative bottom-[4rem] flex flex-col gap-y-2">
+        <div className="relative bottom-[4rem] flex flex-col gap-y-4 items-center">
           {users.map((u) => (
             <Link key={u.id.toString()} href={`/profile/${u.id}`}>
-              <Button className="items-center justify-center rounded-md rightsidebar-item border-none">
+              <Button variant={"outline"} className="rounded-full likebutton items-center justify-center bg-transparent outline-blue border-none">
                 {u.name}
                 {u.score !== undefined && (
                   <span className="ml-2 text-xs text-gray-600">
