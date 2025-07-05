5ea920293e30739ad78e0b2c0782bebaefacb223
Rohan Mathur <161398429+rohan-k-mathur@users.noreply.github.com>
2025-07-04 22:13:51 -0700

    Fix Gmail example modal

--- app/(root)/(standard)/workflows/example/page.tsx
+++ app/(root)/(standard)/workflows/example/page.tsx
@@ -2,6 +2,14 @@
 
 import CounterOutputExample from "@/components/workflow/examples/CounterOutputExample";
 import GmailFlowExample from "@/components/workflow/examples/GmailFlowExample";
+import { ReactFlowProvider } from "@xyflow/react";
+import Modal from "@/components/modals/Modal";
+
 export default function Page() {
-  return <GmailFlowExample />;
+  return (
+    <ReactFlowProvider>
+      <Modal />
+      <GmailFlowExample />
+    </ReactFlowProvider>
+  );
 }
