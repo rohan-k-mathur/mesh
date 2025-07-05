f12faedbf0a64942c52622249d8a46886c24e3f2
Rohan Mathur <161398429+rohan-k-mathur@users.noreply.github.com>
2025-07-05 12:57:12 -0700

    Fix plugin loader

--- lib/pluginImporters.ts
+++ lib/pluginImporters.ts
@@ -1,25 +1,16 @@
 import { PluginDescriptor } from "./pluginLoader";
 
+const pluginFiles = [
+  "CounterNode.tsx",
+  "HelloNode.tsx",
+  "PdfViewerNode.tsx",
+  "SplineViewerNode.tsx",
+];
+
 const importers: Record<string, () => Promise<{ descriptor?: PluginDescriptor }>> = {};
 
-declare const require: any;
-
-if (typeof require !== "undefined" && require.context) {
-  console.log("pluginImporters: using require.context");
-  const ctx = require.context("../plugins", false, /\\.tsx$/);
-  ctx.keys().forEach((key: string) => {
-    importers[key] = () => Promise.resolve(ctx(key));
-  });
-  console.log("pluginImporters: loaded", Object.keys(importers));
-} else if (typeof import.meta !== "undefined" && (import.meta as any).glob) {
-  console.log("pluginImporters: using import.meta.glob");
-  const modules = (import.meta as any).glob("../plugins/*.tsx");
-  Object.keys(modules).forEach((key) => {
-    importers[key] = modules[key] as () => Promise<{ descriptor?: PluginDescriptor }>;
-  });
-  console.log("pluginImporters: loaded", Object.keys(importers));
-} else {
-  console.log("pluginImporters: no loader api found");
-}
+pluginFiles.forEach((file) => {
+  importers[`../plugins/${file}`] = () => import(`../plugins/${file}`);
+});
 
 export default importers;
