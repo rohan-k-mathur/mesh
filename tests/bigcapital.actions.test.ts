88d7b535540fb363cc754da478a2c4ca73c7322b
Rohan Mathur <161398429+rohan-k-mathur@users.noreply.github.com>
2025-07-10 21:32:59 -0700

    Remove bigcapital integration

--- tests/bigcapital.actions.test.ts
+++
@@ -1,6 +0,0 @@
-describe("bigcapital actions", () => {
-  it("exports createInvoice", async () => {
-    const { createInvoice } = await import("@/lib/actions/bigcapital.actions");
-    expect(typeof createInvoice).toBe("function");
-  });
-});
