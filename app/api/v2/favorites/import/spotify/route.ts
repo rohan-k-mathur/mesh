c1b737f658ccd29eb8d80127cb0fa8bb8f60e432
Rohan Mathur <rohan.kumar.mathur@gmail.com>
2025-07-15 14:50:32 -0700

    Update route.ts

--- app/api/v2/favorites/import/spotify/route.ts
+++ app/api/v2/favorites/import/spotify/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs';  
+
 import { NextRequest, NextResponse } from "next/server";
 import { getUserFromCookies } from "@/lib/serverutils";
 import { prisma } from "@/lib/prismaclient";
